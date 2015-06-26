a12bda3bcdff01ec6406ac0446ef282632d2c8c5
apopma <adam.popma@gmail.com>
2015-06-26 12:36:21 -0700

    finish PolyTreeNode

---
+++ w01/d5/w1d5/00_tree_node.rb
@@ -0,0 +1,55 @@
+class PolyTreeNode
+  attr_accessor :value
+  attr_reader :children, :parent
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent)
+    if new_parent.nil?
+      @parent = nil
+      return
+    end
+
+    @parent.children.delete(self) unless @parent.nil?
+    unless new_parent.children.include?(self) && @parent
+      new_parent.children << self
+    end
+
+    @parent = new_parent
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child)
+    child.parent = nil
+    raise "Node is not this node's child" unless self.children.include?(child)
+  end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+
+    @children.each do |child_node|
+      result = child_node.dfs(target_value)
+      return result if result
+    end
+
+    nil
+  end
+
+  def bfs(target_value)
+    queue = []
+    queue << self
+
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+      queue.concat(current_node.children)
+    end
+  end
+end
